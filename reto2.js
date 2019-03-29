0375e4e3166af5ced2874449da3a6aeb2fedf94a
Alejandro Zuñiga <alejandrozuniga@MacBook-Pro-de-Alejandro.local>
2019-03-28 21:35:54 -0500

    Otros ejercicios

--- reto2.js
+++ reto2.js
@@ -1,40 +1,111 @@
+/**
+ * Ejercicio 2
+ */
+function calcularPi() {
+    var numeroTerminos = 10000;
+    var resultado = 2;
+    for (var i = 1; i <= numeroTerminos; i++) {
+        resultado *= ((2 * i) / ((2 * i) - 1)) * ((2 * i) / ((2 * i) + 1));
+    }
+    console.log(resultado);
+}
+
+/**
+ * Ejercicio 4
+ */
+
+function obtenerDomingoPascua() {
+    var maximoAnio = 2000;
+    var minAnio = 1990;
+    var resultado = "";
+    var a = 0;
+    var b = 0;
+    var c = 0;
+    var d = 0;
+    var e = 0;
+    for (var i = minAnio; i <= maximoAnio; i++) {
+        a = i % 19;
+        b = i % 4;
+        c = i % 7;
+        d = (19 * a + 24) % 30;
+        e = (2 * b + 4 * c + 6 * d + 5) % 7;
+        resultado += i + " -> " + calcularFecha(22 + d + e)
+            + "<br>";
+    }
+    console.log(resultado);
+}
+
+function calcularFecha(dias) {
+    if (dias > 31) {
+        return (dias - 31) + " de abril";
+    }
+    return dias + " de marzo";
+}
+
+
+
+/**
+ * Ejercicio 8
+ */
+function calcularEjercicio8() {
+    var x = 2;//ingresar
+    var maxPotencia = 100;
+    var i = 1;
+    var resultado = 1;
+    for (i; i <= maxPotencia; i++) {
+        resultado += (Math.pow(x, i) / factorial(i));
+    }
+    console.log(resultado);
+}
+
+function factorial(n) {
+    var resultado = 1;
+    var i = 1;
+    for (i; i <= n; i++) {
+        resultado *= i
+    }
+    return resultado;
+}
+
 
 /**
  * Ejercicio 14
  */
-function mostrarLineas(){
+function mostrarLineas() {
     var lineas = 10;
     var grafica = '';
     var i = 0;
-    for(i; i <= lineas; i++){
+    for (i; i <= lineas; i++) {
         var j = 0;
-        for(j; j < i; j++){
-            grafica+="*"
+        for (j; j < i; j++) {
+            grafica += "*"
         }
-        grafica+="<br>";
+        grafica += "<br>";
     }
     console.log(grafica);
 }
 
 /**
- * Ejercicio 8
+ * Ejercicio 15 
  */
-function calcularEjercicio8(){
-    var x = 2;//ingresar
-    var maxPotencia = 100;
-    var i = 1;
-    var resultado = 1;
-    for(i; i <=maxPotencia;i++){
-        resultado+=(Math.pow(x,i)/factorial(i));
+function listarNumerosPerfectos() {
+    var numeroLimite = 10000;
+    var resultado = "";
+    for(var i = 1; i < numeroLimite;i++){
+        if(validarNumeroPerfecto(i)){
+            resultado+=" " + i;
+        }
     }
     console.log(resultado);
 }
 
-function factorial(n){
-    var resultado = 1;
-    var i = 1;
-    for(i; i<=n;i++){
-        resultado*=i
+function validarNumeroPerfecto(numero) {
+    var mitad = numero / 2;
+    var suma = 0;
+    for (var i = 1; i <= mitad; i++) {
+        if (numero % i === 0) {
+            suma += i;
+        }
     }
-    return resultado;
+    return suma === numero;
 }
